f46d24eebc9bae3251f5fd6f84d37f7dc6774f3d
THOMAS <thomaslunardo@gmail.com>
2023-11-24 00:13:58 +0100

    mignight debugg

--- src/Controller/TouchController.php
+++ src/Controller/TouchController.php
@@ -3,13 +3,15 @@
 namespace App\Controller;
 
 use App\Model\TouchManager;
+use App\Model\LoginManager;
+use App\Model\ChooseManager;
 
 class TouchController extends AbstractController
 {
     /**
      * Display home page
      */
-    public function touch(int $id): string
+    public function touch($id): string
     {
         $touchManager = new TouchManager();
         $toucher = $touchManager->touchById($id);
---
+++ src/Model/GiftManager.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Model;
+
+use App\Model\AbstractManager;
+use PDO;
+
+class GiftManager extends AbstractManager
+{
+    protected PDO $pdo;
+
+    public const TABLE = 'gift';
+
+    public function getTotalGiftsInBdd(): int
+    {
+        $statement = $this->pdo->query("SELECT COUNT(*) FROM " . static::TABLE);
+        $totalGiftInBdd = $statement->fetch(PDO::FETCH_ASSOC);
+
+        return $totalGiftInBdd['total'] ?? 0;
+    }
+
+    public function selectRandomGift(): ?int
+    {
+        $totalGifts = $this->getTotalGiftsInBdd();
+
+        if ($totalGifts > 0) {
+            $randomId = random_int(1, $totalGifts);
+
+            $statement = $this->pdo->prepare("SELECT * FROM " .
+            static::TABLE . " WHERE id=:id ORDER BY RAND() LIMIT 1");
+            $statement->bindValue('id', $randomId, PDO::PARAM_INT);
+            $statement->execute();
+
+            return $statement->fetch();
+        } else {
+            return null;
+        }
+    }
+}
