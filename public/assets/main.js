449bb7e8b150dbc0a51d40fc52807c6f8999bfaf
NICKY RASAMOELINA <nickyrasa@gmail.com>
2023-11-23 18:53:08 +0100

    modal

--- public/assets/main.js
+++ public/assets/main.js
@@ -107,23 +107,23 @@ function randomInRange(min, max) {
 
   skew = Math.max(0.8, skew - 0.001);
 
-  confetti({
-    particleCount: 1,
-    startVelocity: 0,
-    ticks: ticks,
-    origin: {
-      x: Math.random(),
-      // since particles fall down, skew start toward the top
-      y: Math.random() * skew - 0.2,
-    },
-    colors: ["#ffffff"],
-    shapes: ["circle"],
-    gravity: randomInRange(0.4, 0.6),
-    scalar: randomInRange(0.4, 1),
-    drift: randomInRange(-0.4, 0.4),
-  });
+//   confetti({
+//     particleCount: 1,
+//     startVelocity: 0,
+//     ticks: ticks,
+//     origin: {
+//       x: Math.random(),
+//       // since particles fall down, skew start toward the top
+//       y: Math.random() * skew - 0.2,
+//     },
+//     colors: ["#ffffff"],
+//     shapes: ["circle"],
+//     gravity: randomInRange(0.4, 0.6),
+//     scalar: randomInRange(0.4, 1),
+//     drift: randomInRange(-0.4, 0.4),
+//   });
 
-  if (timeLeft > 0) {
-    requestAnimationFrame(frame);
-  }
+//   if (timeLeft > 0) {
+//     requestAnimationFrame(frame);
+//   }
 })();
